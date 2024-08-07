849c145cc32edf1421a445af8194cbcd5139c7c1
standvan <hovantam2509@gmail.com>
2024-08-07 18:54:32 +0700

    add host server

--- ui/src/redux/apis/Axios.js
+++ ui/src/redux/apis/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const Axios = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://netflix-clone-super-server.onrender.com/api",
 });
 export default Axios;
